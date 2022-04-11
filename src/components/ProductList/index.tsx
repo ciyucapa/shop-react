af1fedfbb05eb80387851ee3139bbeebebfabe2d
ciyucapa <cindycaceres134@gmail.com>
2022-04-11 10:00:56 -0500

    :sparkles: feat: productlist

---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,11 @@
+const ProductList = () => {
+    return (
+        <section className="productList__container">
+            <div>
+                productos
+            </div>
+        </section>
+    )
+};
+
+export default ProductList;
